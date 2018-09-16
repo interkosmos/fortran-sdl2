991b85b5fb95b20be701d0fa162c899eafe26113
interkosmos <pengel@hs-nb.de>
2018-09-16 20:17:17 +0200

    Added SDL_RenderText_Shaded().

--- sdl2_ttf.f90
+++ sdl2_ttf.f90
@@ -11,6 +11,7 @@ module sdl2_ttf
 
     public :: ttf_init
     public :: ttf_open_font
+    public :: ttf_render_text_shaded
     public :: ttf_render_text_solid
     public :: ttf_close_font
     public :: ttf_quit
@@ -31,6 +32,18 @@ module sdl2_ttf
             integer(kind=c_int),    intent(in), value :: ptsize
             type(c_ptr)                               :: ttf_open_font
         end function ttf_open_font
+
+        ! SDL_Surface *TTF_RenderText_Shaded(TTF_Font *font, const char *text, SDL_Color fg, SDL_Color bg)
+        function ttf_render_text_shaded_(font, text, fg, bg) bind(c, name='TTF_RenderText_Shaded')
+            use, intrinsic :: iso_c_binding
+            use :: sdl2_types
+            implicit none
+            type(c_ptr),            intent(in), value :: font
+            character(kind=c_char), intent(in)        :: text
+            type(sdl_color),        intent(in), value :: fg
+            type(sdl_color),        intent(in), value :: bg
+            type(c_ptr)                               :: ttf_render_text_shaded_
+        end function ttf_render_text_shaded_
 
         ! SDL_Surface *TTF_RenderText_Solid(TTF_Font *font, const char *text, SDL_Color fg)
         function ttf_render_text_solid_(font, text, fg) bind(c, name='TTF_RenderText_Solid')
@@ -56,6 +69,24 @@ module sdl2_ttf
     end interface
 
     contains
+        ! SDL_Surface *TTF_RenderText_Shaded(TTF_Font *font, const char *text, SDL_Color fg, SDL_Color bg)
+        function ttf_render_text_shaded(font, text, fg, bg)
+            !! Calls `ttf_render_text_shaded_()` and converts the returned
+            !! C pointer to derived type `sdl_surface`.
+            use, intrinsic :: iso_c_binding
+            use :: sdl2_types
+            implicit none
+            type(c_ptr),            intent(in) :: font
+            character(kind=c_char), intent(in) :: text
+            type(sdl_color),        intent(in) :: fg
+            type(sdl_color),        intent(in) :: bg
+            type(sdl_surface),      pointer    :: ttf_render_text_shaded
+            type(c_ptr)                        :: ptr
+
+            ptr = ttf_render_text_shaded_(font, text, fg, bg)
+            call c_f_pointer(ptr, ttf_render_text_shaded)
+        end function ttf_render_text_shaded
+
         ! SDL_Surface *TTF_RenderText_Solid(TTF_Font *font, const char *text, SDL_Color fg)
         function ttf_render_text_solid(font, text, fg)
             !! Calls `ttf_render_text_solid_()` and converts the returned
