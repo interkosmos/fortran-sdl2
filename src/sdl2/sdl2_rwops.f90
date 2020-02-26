aa703221bf913eadc0940a60059015be678bab44
interkosmos <pengel@hs-nb.de>
2020-02-26 20:09:29 +0100

    Added SDL_RWFromConstMem().

--- src/sdl2/sdl2_rwops.f90
+++ src/sdl2/sdl2_rwops.f90
@@ -9,10 +9,19 @@ module sdl2_rwops
     use, intrinsic :: iso_c_binding, only: c_char, c_int, c_ptr
     implicit none
 
+    public :: sdl_rw_from_const_mem
     public :: sdl_rw_from_file
     public :: sdl_rw_from_mem
 
     interface
+        ! SDL_RWops *SDL_RWFromConstMem(void *mem, int size)
+        function sdl_rw_from_const_mem(mem, size) bind(c, name='SDL_RWFromConstMem')
+            import :: c_ptr, c_int
+            type(c_ptr),         intent(in), value :: mem
+            integer(kind=c_int), intent(in), value :: size
+            type(c_ptr)                            :: sdl_rw_from_const_mem
+        end function sdl_rw_from_const_mem
+
         ! SDL_RWops *SDL_RWFromFile(const char *file, const char *mode)
         function sdl_rw_from_file(file, mode) bind(c, name='SDL_RWFromFile')
             import :: c_char, c_ptr
