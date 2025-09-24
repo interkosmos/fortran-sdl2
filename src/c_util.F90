81a3c8ee562073a78cdfad6537c0d57c2f642aa9
Philipp <pengel@hs-nb.de>
2025-09-24 12:10:19 +0200

    Fix GNU Fortran 15 compatibility.

--- src/c_util.F90
+++ src/c_util.F90
@@ -6,11 +6,13 @@
 ! GitHub:  https://github.com/interkosmos/fortran-sdl2/
 ! Licence: ISC
 module c_util
-    use, intrinsic :: iso_c_binding
-    implicit none
-    private
 
 #if defined (__flang__) || (defined (__GFORTRAN__) && __GNUC__ > 15) || (defined (__GFORTRAN__) && __GNUC__ == 15 && __GNUC_MINOR__ >= 2)
+
+    use, intrinsic :: iso_c_binding, only: c_uint8_t, c_uint16_t, c_uint32_t, c_uint64_t, c_unsigned, c_unsigned_char, &
+                                           c_unsigned_long, c_unsigned_long_long, c_unsigned_short
+    implicit none
+    private
 
     public :: c_uint8_t
     public :: c_uint16_t
@@ -23,6 +25,11 @@ module c_util
     public :: c_unsigned_short
 
 #else
+
+    use, intrinsic :: iso_c_binding, only: c_int8_t, c_int16_t, c_int32_t, c_int64_t, c_int, c_signed_char, &
+                                           c_long, c_long_long, c_short
+    implicit none
+    private
 
     integer, parameter, public :: c_uint8_t            = c_int8_t
     integer, parameter, public :: c_uint16_t           = c_int16_t
@@ -41,7 +48,7 @@ module c_util
     ! Foreign function interfaces to libc.
     interface
         function c_strlen(str) bind(c, name='strlen')
-            import :: c_ptr, c_size_t
+            use, intrinsic :: iso_c_binding, only: c_ptr, c_size_t
             type(c_ptr), intent(in), value :: str
             integer(kind=c_size_t)         :: c_strlen
         end function c_strlen
